6f6527a41dd97691db08aa38e7ac367e2df95747
Rajesh Lakkakula <rajeshlakkakula536@gmail.com>
2022-10-17 07:47:30 +0530

    This Commit contains homefile changes with  list of products

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shrine_app/model/ProductRepo.dart';
+import 'package:shrine_app/model/product.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -37,14 +40,23 @@ class HomePage extends StatelessWidget {
         ),
         body: GridView.count(
             crossAxisCount: 2,
-            childAspectRatio: 8/9,
+            childAspectRatio: 8 / 9,
             padding: EdgeInsets.all(16),
-            children: _buildGridCards(10))
-    );
+            children: _buildGridCards(context)));
   }
 
-  List<Card> _buildGridCards(int count) {
-    List<Card> cards = List.generate(count, (int index) {
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductRepo.loadProducts(Category.all);
+
+    if (products.isEmpty) {
+      return <Card>[];
+    }
+    final ThemeData themeData = Theme.of(context);
+
+    final NumberFormat format = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+
+    return products.map((product) {
       return Card(
         clipBehavior: Clip.antiAlias,
         child: Column(
@@ -52,26 +64,35 @@ class HomePage extends StatelessWidget {
           children: [
             AspectRatio(
               aspectRatio: 18.0 / 11.0,
-              child: Image.asset('assets/diamond.png'),
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.fitWidth,
+              ),
             ),
             Padding(
               padding: EdgeInsets.fromLTRB(16, 12, 16, 8),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Title'),
+                  Text(
+                    product.name,
+                    style: themeData.textTheme.headline6,
+                    maxLines: 1,
+                  ),
                   SizedBox(
                     height: 8,
                   ),
-                  Text('Secondary Text')
+                  Text(
+                    format.format(product.price),
+                    style: themeData.textTheme.subtitle2,
+                  )
                 ],
               ),
             )
           ],
         ),
       );
-    });
-
-    return cards;
+    }).toList();
   }
 }
